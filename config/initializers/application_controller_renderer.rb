9ddcb33bd1f1b0e4ed154cd46726e09a9c41daa8
tomo32 <19da018k@rikkyo.ac.jp>
2021-02-27 10:28:07 +0000

    [Finish] 5-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ef0ba271cb720c5e4211bc5a5c142dea4e1c86af908881101ec0eabfd1040f7f21578f708c291fd074967b5bbcb5b99411f32d25d96e47df8699ce7ccb0aa2e1'
